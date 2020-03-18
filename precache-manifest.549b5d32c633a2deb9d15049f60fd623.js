21f009b5f0e3dca05d4e42e52548edfa9d5c5b8e
Esther <ueagbaji@wpi.edu>
2020-03-17 21:39:12 -0400

    Updates

---
+++ precache-manifest.549b5d32c633a2deb9d15049f60fd623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1535c900a76287c5524e3b96677169a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "eebc509bccc82624483b",
+    "url": "/monsters-rolodex/static/css/main.26c2d674.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebc509bccc82624483b",
+    "url": "/monsters-rolodex/static/js/main.596597d8.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
